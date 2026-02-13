895eb7c314cf2b4aa8b53569a56703376036bbfe
Razvan Stefan Morar <business@razvanmorar.com>
2026-02-13 16:30:17 +0200

    Refactor AuthorRepository: Extend ReactiveCrudRepository and add custom query

--- src/main/java/com/crud/exampleapp/Model/Repos/AuthorRepository.java
+++ src/main/java/com/crud/exampleapp/Model/Repos/AuthorRepository.java
@@ -1,16 +1,9 @@
 package com.crud.exampleapp.Model.Repos;
 
-
 import com.crud.exampleapp.Model.Beans.Author;
-import reactor.core.publisher.Mono;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
-
-
-public interface AuthorRepository {
-
-    Mono<Author> save(Author author);
-
-    Mono<Author> findById(String id);
-
-    Mono<String> delete(String id);
+public interface AuthorRepository extends ReactiveCrudRepository<Author, String> {
+    Flux<Author> findByNameContaining(String name);
 }
